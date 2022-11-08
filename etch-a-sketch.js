6546196cbb863a53ace3ea607ced2bbbb1075f58
Brett Parker <absentminded.lich@gmail.com>
2022-11-07 21:48:31 -0500

    Add rand color and lower opacity

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -14,7 +14,10 @@ function createTile(width, height = width) {
     tile.style.height = height;
     tile.classList.add('tile');
 
-    tile.addEventListener('mouseover', () => tile.classList.add('tile-on'));
+    tile.addEventListener('mouseover', () => {
+        tile.style.backgroundColor = getRandColor();
+        decreaseOpacity(tile);
+    });
 
     return tile;
 }
@@ -27,6 +30,18 @@ function getBoard() {
 function getDOMHeight() {
     return Math.max(document.body.scrollHeight, document.body.offsetHeight,
         document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight);
+}
+
+function getRandColor() {
+    return Math.floor(Math.random() * 16777215).toString(16);
+}
+
+function decreaseOpacity(tile, mag = 0.1) {
+    if (tile.style.opacity === '') {
+        tile.style.opacity = 1;
+    } else if (tile.style.opacity > 0) {
+        tile.style.opacity -= mag;
+    }
 }
 
 function populateBoard(board, horzTiles = 16, vertTiles = horzTiles) {
